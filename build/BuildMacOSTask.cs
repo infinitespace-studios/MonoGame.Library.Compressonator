96a9888af97dfd5e669eac70168c9387fe9b7825
Dean Ellis <dellis1972@googlemail.com>
2024-03-15 12:36:35 +0000

    move python to main.yml

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -12,7 +12,6 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "compressonator/";
-        context.StartProcess("python3", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "build/fetch_dependencies.py" });
         context.StartProcess ("sh", new ProcessSettings { WorkingDirectory = buildWorkingDir , Arguments = "build/initsetup_mac.sh" });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_APPS_CMP_CLI=ON -DOPTION_BUILD_ASTC=ON -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -12,7 +12,6 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "compressonator";
-        context.StartProcess("python3", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "build/fetch_dependencies.py"});
         context.StartProcess ("build/windows_build_sdk.bat", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_APPS_CMP_CLI=ON -DOPTION_BUILD_ASTC=ON -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
