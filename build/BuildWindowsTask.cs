27fedc9ae220dc4b1d9fe360bf3ef514913ffb4d
Dean Ellis <dellis1972@googlemail.com>
2024-03-15 11:54:32 +0000

    Fix up windows and linux builds a bit

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -13,10 +13,10 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     {
         var buildWorkingDir = "compressonator";
         context.StartProcess("python3", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "build/fetch_dependencies.py"});
-        context.StartProcess ("scripts/windows_build_sdk.bat", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
+        context.StartProcess ("build/windows_build_sdk.bat", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_APPS_CMP_CLI=ON -DOPTION_BUILD_ASTC=ON -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
-        var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "libcompressonator.*.*.*.so", SearchOption.TopDirectoryOnly);
-        context.CopyFile(files[0], $"{context.ArtifactsDir}/libcompressonator.so");
+        var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "libcompressonator.*.*.*.dll", SearchOption.TopDirectoryOnly);
+        context.CopyFile(files[0], $"{context.ArtifactsDir}/libcompressonator.dll");
     }
 }
