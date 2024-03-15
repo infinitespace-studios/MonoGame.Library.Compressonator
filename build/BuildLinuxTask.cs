0bccbce93119a67a2f9398e517fea3371db70db5
Dean Ellis <dellis1972@googlemail.com>
2024-03-15 14:04:41 +0000

    try this

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -13,7 +13,7 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
     {
         var buildWorkingDir = "compressonator/";
         context.StartProcess ("sh", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "build/initsetup_ubuntu.sh" });
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_APPS_CMP_CLI=ON -DOPTION_BUILD_ASTC=ON -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_APPS_CMP_CLI=ON -DOPTION_BUILD_ASTC=ON CMakeLists.txt" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "libcompressonator.*.*.*.so", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/libcompressonator.so");
